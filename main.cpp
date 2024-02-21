f5223df5da47e931c5b7121d10f28d1110f2b8aa
runicoZH <nicoruckstuhl@gmx.ch>
2024-02-21 17:26:47 +0100

    visualizing process and result

--- main.cpp
+++ main.cpp
@@ -51,12 +51,52 @@ cv::Mat edge_detection(const cv::Mat &frame) {
     return edge_image;
 }
 
-void display(const cv::Mat &frame) {
-    cv::Mat resized_frame;
-    cv::resize(frame, resized_frame, cv::Size(920, 540));
-    cv::imshow("display", resized_frame);
+void display_process(const cv::Mat &base_image, const cv::Mat &filtered_image, const cv::Mat &masked_image, const cv::Mat &line_image) {
+    cv::Mat resized_base_image;
+    cv::resize(base_image, resized_base_image, cv::Size(960, 540));
+    
+    cv::Mat resized_filtered_image;
+    cv::resize(filtered_image, resized_filtered_image, cv::Size(960, 540));
+
+    cv::Mat resized_masked_image;
+    cv::resize(masked_image, resized_masked_image, cv::Size(960, 540));
+
+    cv::Mat color_edge_image;
+    cv::cvtColor(line_image, color_edge_image, cv::COLOR_GRAY2BGR);
+
+    cv::Mat resized_line_image;
+    cv::resize(color_edge_image, resized_line_image, cv::Size(960, 540));
+
+    cv::Mat top_images;
+    cv::Mat bot_images;
+    cv::Mat all_images;
+    cv::hconcat(resized_base_image, resized_filtered_image, top_images);
+    cv::hconcat(resized_masked_image, resized_line_image, bot_images);
+    cv::vconcat(top_images, bot_images, all_images);
+
+    cv::imshow("process", all_images);
     cv::waitKey(25);
 }
+
+void display_result(const cv::Mat &base_image, cv::Vec4i left_line, cv::Vec4i right_line){
+
+    cv::Mat result_image = base_image;
+
+    cv::line(result_image, cv::Point(left_line[0], left_line[1]),
+                 cv::Point(left_line[2], left_line[3]), (0, 255, 0), 10,
+                 cv::LINE_AA);
+    cv::line(result_image, cv::Point(right_line[0], right_line[1]),
+                cv::Point(right_line[2], right_line[3]), (0, 0, 255), 10,
+                cv::LINE_AA);
+
+
+    cv::Mat resized_result_image;
+    cv::resize(result_image, resized_result_image, cv::Size(960, 540));
+
+    cv::imshow("Result", resized_result_image);
+    cv::waitKey(25);
+}
+
 
 int main(int argc, char *argv[]) {
     cv::Mat frame;
@@ -79,15 +119,13 @@ int main(int argc, char *argv[]) {
         cv::Mat filtered_image = filter_image(frame);
         cv::Mat masked_image = mask_image(filtered_image);
         cv::Mat edge_image = edge_detection(masked_image);
+
         auto [left_line, right_line] = line_detector.find_lines(edge_image);
 
-        cv::line(frame, cv::Point(left_line[0], left_line[1]),
-                 cv::Point(left_line[2], left_line[3]), (0, 255, 0), 10,
-                 cv::LINE_AA);
-        cv::line(frame, cv::Point(right_line[0], right_line[1]),
-                 cv::Point(right_line[2], right_line[3]), (0, 0, 255), 10,
-                 cv::LINE_AA);
-        display(frame);
+        display_process(frame, filtered_image, masked_image, edge_image);
+        display_result(frame, left_line, right_line);
+        
+
     }
 
     return 0;
